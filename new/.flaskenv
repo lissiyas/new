dc7e1014e361cc0d8c042338be09bf2671e193f5
lissiyas <62198945+lissiyas@users.noreply.github.com>
2020-04-10 14:37:28 +0530

    second commit

---
+++ new/.flaskenv
@@ -0,0 +1,2 @@
+FLASK_env=development
+FLASK_APP=flask_qa
---
+++ new/flask_qa/commands.py
@@ -0,0 +1,14 @@
+import click
+from flask import Blueprint
+from flask.cli import with_appcontext
+
+from .extensions import db
+from .models import User, Question
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
+
+
+    
